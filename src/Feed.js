edf577afeb7e60d27fe057f52eb2c800fe1eb9fd
Priam Jain <priamjain8@gmail.com>
2020-08-12 15:22:37 +0530

    TweetBox

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Feed.css';
+import TweetBox from './TweetBox'
 function Feed() {
 	return (
 		<div className='feed'>
 			<h2 className="feed__header">Home</h2>
+			<TweetBox></TweetBox>
+			<p>abc</p>
+			<p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p><p>abc</p>
 		</div>
+
 	)
 }
 
---
+++ src/TweetBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TweetBox.css'
+function TweetBox() {
+	return (
+		<div className='tweetBox'>
+			<form action="">
+
+			</form>
+			<button className='tweetBox__tweetbutton'>Tweet</button>
+		</div>
+	)
+}
+
+export default TweetBox
