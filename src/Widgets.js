d4eec7efa968b523f9fb93c88d53789a7ba6f57d
Priam Jain <priamjain8@gmail.com>
2020-08-15 14:23:27 +0530

    Widgets completed

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Widgets.css'
+import {TwitterTweetEmbed,TwitterTimelineEmbed} from 'react-twitter-embed'
 function Widgets() {
 	return (
 		<div className='widgets'>
-			Widget
+			<div className="widget__input">
+				<input type="text" placeholder="Search Twitter" class="widget__search"/>
+			</div>
+			<div className="whatsHappening">			<TwitterTweetEmbed
+				tweetId="1290943803679899648"
+			/>
+			</div>
+			<div className="whatsHappening">	
+			<TwitterTimelineEmbed
+				sourceType="profile"
+				screenName="effervescent22"
+				options={{height: 400}}
+			/>
+			</div>
+
 		</div>
-	)
+		)
 }
 
 export default Widgets
